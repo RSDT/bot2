01a3be6b6052ce324257787949d82378e3d2c37a
njittam <git@mattijnkreuzen.nl>
2017-07-24 00:13:26 +0200

    added setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='bot2',
+    version='',
+    packages=['PythonApi', 'PythonApi.Base', 'PythonApi.RPApi', 'PythonApi.tests', 'PythonApi.jotihunt',
+              'PythonApi.jotihunt2', 'PythonApi.scraperApi'],
+    url='',
+    license='',
+    author='mattijn',
+    author_email='',
+    description='', requires=['requests', 'pyorient', 'python-telegram-bot']
+)
